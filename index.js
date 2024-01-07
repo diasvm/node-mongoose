91e9b97fb369991578122877d4b9dfbf27d0d4ac
Victor Dias <diasvm@hotmail.com>
2024-01-07 00:18:38 -0800

    Mongoose Part3 - Final.

--- index.js
+++ index.js
@@ -3,31 +3,50 @@ const Campsite = require('./models/campsite');
 
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useUnifiedParser: true,
-    useUnifiedTopology: true
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 connect.then( () => { 
     console.log('Connected correctly to server');
 
     Campsite.create({
-        name: 'React Lake Campground',
-        description: 'test'
+      name: "React Lake Campground1",
+      description: "test",
     })
-    .then(campsite => {
+      .then((campsite) => {
         console.log(campsite);
-        return Campsite.find();
-    })
-    .then(campsites => {
-        console.log(campsites);
+        return Campsite.findByIdAndUpdate(
+          campsite._id,
+          {
+            $set: { description: "Updated Test Document" },
+          },
+          {
+            new: true,
+          }
+        );
+      })
+      .then((updatedCampsite) => {
+        console.log(updatedCampsite);
+        updatedCampsite.comments.push({
+          rating: 5,
+          text: "What a magnificent view!",
+          author: "Tinus Lorvaldes",
+        });
+
+        return updatedCampsite.save();
+      })
+      .then((campsite) => {
+        console.log(campsite);
         return Campsite.deleteMany();
-    })
-    .then(() => {
+      })
+      .then(() => {
         mongoose.connection.close();
-    })
-    .catch(err => {
+      })
+      .catch((err) => {
         console.log(err);
-        mongoose.connection.close(); 
-    })
+        mongoose.connection.close();
+      });
 });
--- models/campsite.js
+++ models/campsite.js
@@ -1,8 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const campsiteSchema = new Schema(
-  {
+const commentSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  }
+}, {
+    timestamps: true
+});
+
+const campsiteSchema = new Schema({
     name: {
       type: String,
       required: true,
@@ -12,11 +30,10 @@ const campsiteSchema = new Schema(
       type: String,
       required: true,
     },
-  },
-  {
+    comments: [commentSchema]
+}, {
     timestamps: true,
-  }
-);
+});
 
 const Campsite = mongoose.model("Campsite", campsiteSchema);
 
